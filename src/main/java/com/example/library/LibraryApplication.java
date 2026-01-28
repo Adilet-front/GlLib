21706557b8c773c65abe4628800697fdcba33531
Seitmametov <sseitmametov@gmail.com>
2026-01-28 17:43:35 +0600

    git push

--- src/main/java/com/example/library/LibraryApplication.java
+++ src/main/java/com/example/library/LibraryApplication.java
@@ -1,5 +1,5 @@
 package com.example.library;
-
+//льижжлпьидп
 import com.example.library.config.ReservationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
